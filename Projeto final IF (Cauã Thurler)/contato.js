49480727a8e80e05427c44b4231caccba93a40a7
Cauã Thurler <101412705+Cauatpa@users.noreply.github.com>
2022-05-03 22:06:58 -0300

    Add files via upload

---
+++ Projeto final IF (Cauã Thurler)/contato.js
@@ -0,0 +1,6 @@
+function Enviar(){
+    var nomeCompleto = document.getElementById("nomecompletoid")
+    var email = document.getElementById("emailid")
+    var Telefone = document.getElementById("telid")
+    alert('Nome: ' + nomeCompleto.value + '\n' + 'E-mail: ' + email.value + '\n' + 'Telefone: ' + Telefone.value);
+}
